67e978ab73174f284050805902bbdcad3c6a43eb
Vasya Pupkin <sashasonya2106@gmail.com>
2022-09-01 20:51:02 +0300

    3

--- src/MyName.java
+++ src/MyName.java
@@ -16,14 +16,17 @@ int maxV = min;
                 System.out.print(nums[i][j] + " ");
             }
             System.out.println();
+
+
+            System.out.println();
         }
- /*for (int i=0; i<nums.length ;i++){
+ for (int i=0; i<nums.length ;i++){
             for (int j=0; j<nums[i].length;j++){
 
                 System.out.print(nums[i][j] + " ");
             }
             System.out.println();
-        }*/
+        }
         System.out.println(maxV);
 
 
